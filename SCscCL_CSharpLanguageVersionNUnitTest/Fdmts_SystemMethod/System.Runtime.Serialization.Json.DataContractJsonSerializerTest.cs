3ece7bc1f8909acf33b01ffc136e74c594f5fef6
张少驰 <shaochi.zhang@capgemini.com>
2023-08-14 19:53:25 +0800

    feat: 新增DataContractJsonSerializer示例（参考MSDOC）。注意这是微软推荐的转换方式，安全可靠。

--- SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_SystemMethod/System.Runtime.Serialization.Json.DataContractJsonSerializerTest.cs
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_SystemMethod/System.Runtime.Serialization.Json.DataContractJsonSerializerTest.cs
@@ -1,17 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace CsLangVersion.Fdmts_SystemMethod
 {
 	internal partial class SystemMethodTest
 	{
-		[Test]
-		public void UseDataContractJsonSerializer_序列化和反序列化()
-		{
 
+		[DataContract]
+		private class Person
+		{
+			[DataMember]
+			internal string name;
+
+			[DataMember]
+			internal int age;
+		}
+		/// <summary>
+		/// 参考链接：https://learn.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-serialize-and-deserialize-json-data
+		/// 这种方式更安全：https://learn.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-security-guide#preferred-alternatives
+		/// </summary>
+		[Test]
+		public void UseDataContractJsonSerializer_序列化()
+		{
+			var p = new Person();
+			p.name = "John";
+			p.age = 42;
+
+			var stream1 = new MemoryStream();
+			var ser = new DataContractJsonSerializer(typeof(Person));
+
+			ser.WriteObject(stream1, p);
+
+			stream1.Position = 0;
+			var sr = new StreamReader(stream1);
+			Console.Write("JSON form of Person object: ");
+			Console.WriteLine(sr.ReadToEnd());
+		}
+
+		/// <summary>
+		/// 参考链接：https://learn.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-serialize-and-deserialize-json-data
+		/// </summary>
+		[Test]
+		public void UseDataContractJsonSerializer_反序列化()
+		{
+			var p = new Person();
+			p.name = "John";
+			p.age = 42;
+
+			var stream1 = new MemoryStream();
+			var ser = new DataContractJsonSerializer(typeof(Person));
+
+			ser.WriteObject(stream1, p);
+
+			stream1.Position = 0;
+			var p2 = (Person)ser.ReadObject(stream1);
+
+			Console.WriteLine($"Deserialized back, got name={p2.name}, age={p2.age}");
 		}
 	}
 }
