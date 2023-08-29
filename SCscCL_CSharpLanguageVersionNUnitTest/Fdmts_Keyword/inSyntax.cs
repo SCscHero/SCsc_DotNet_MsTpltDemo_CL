fec35bba5106b867063362ff94db9dbc2f26362d
张少驰 <shaochi.zhang@capgemini.com>
2023-08-30 00:25:14 +0800

    feat: 新增in/ref关键字示例。

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Keyword/inSyntax.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CsLangVersion.Fdmts_Keyword
+{
+    /// <summary>
+    /// https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/keywords/in-parameter-modifier
+    /// 
+    /// </summary>
+    internal class inSyntax
+    {
+        //1.in 关键字会导致按引用传递参数，但确保未修改参数。 它让形参成为实参的别名，这必须是变量。
+        //2.换而言之，对形参执行的任何操作都是对实参执行的。
+        //3.它类似于 ref 或 out 关键字，不同之处在于 in 参数无法通过调用的方法进行修改。 out 参数必须由调用的方法进行修改，这些修改在调用上下文中是可观察的，而 ref 参数是可以修改的。
+        [Test]
+        public void UseIn_引用类型传递参数示例1()
+        {
+            int readonlyArgument = 44;
+            InArgExample(readonlyArgument);
+            Console.WriteLine(readonlyArgument);     // value is still 44
+
+            void InArgExample(in int number)
+            {
+                // Uncomment the following line to see error CS8331
+                //number = 19;
+            }
+        }
+        //in 关键字还作为 foreach 语句的一部分，或作为 LINQ 查询中 join 子句的一部分，与泛型类型参数一起使用来指定该类型参数为逆变。 有关在这些上下文使用 in 关键字的详细信息，请参阅 in，其中提供了所有这些用法的链接。
+        [Test]
+        public void TODO_UseIn_Foreach和Linq当中的示例()
+        {
+
+        }
+        //作为 in 参数传递的变量在方法调用中传递之前必须进行初始化。 但是，所调用的方法可能不会分配值或修改参数。
+        //尽管 in、out 和 ref 参数修饰符被视为签名的一部分，但在单个类型中声明的成员不能仅因 in、ref 和 out 而签名不同。 因此，如果唯一的不同是一个方法采用 ref 或 out 参数，而另一个方法采用 in 参数，则无法重载这两个方法。 例如，以下代码将不会编译：
+        //private class CS0663_Example
+        //{
+        //    // Compiler error CS0663: "Cannot define overloaded
+        //    // methods that differ only on in, ref and out".
+        //    public void SampleMethod(in int i) { }
+        //    public void SampleMethod(ref int i) { }
+        //}
+        [Test]
+        public void UseInAndref_无法重载的编译错误示例()
+        {
+
+        }
+        private class InOverloads
+        {
+            public void SampleMethod(in int i) { }
+            public void SampleMethod(int i) { }
+        }
+        [Test]
+        public void UseInAndref_可以重载的编译正确示例()
+        {
+
+        }
+        private static void Method(int argument)
+        {
+            // implementation removed
+        }
+
+        private static void Method(in int argument)
+        {
+            // implementation removed
+        }
+        [Test]
+        public void UseIn_值类型使用示例()
+        {
+            Method(5); // Calls overload passed by value
+            //Method(5L); // CS1503: no implicit conversion from long to int
+            short s = 0;
+            Method(s); // Calls overload passed by value.
+            //Method(in s); // CS1503: cannot convert from in short to in int
+            int i = 42;
+            Method(i); // Calls overload passed by value
+            Method(in i); // passed by readonly reference, explicitly using `in`
+        }
+    }
+}
---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Keyword/refSyntax.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace CsLangVersion.Fdmts_Keyword
+{
+    /// <summary>
+    /// 将引用类型根据引用传递，而不是值或特定作用域。
+    /// https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/keywords/ref
+    /// </summary>
+    internal class refSyntax
+    {
+        [Test]
+        public void UseRef_按引用传递参数示例1()
+        {
+            void Method(ref int refArgument)
+            {
+                refArgument = refArgument + 44;
+            }
+            //传递到 ref 或 in 形参的实参必须先经过初始化，然后才能传递。 该要求与 out 形参不同，在传递之前，不需要显式初始化该形参的实参。
+            int number = 1;
+            Method(ref number);
+            Console.WriteLine(number);
+            // Output: 45
+        }
+        private class Product
+        {
+            public Product(string name, int newID)
+            {
+                ItemName = name;
+                ItemID = newID;
+            }
+            public string ItemName { get; set; }
+            public int ItemID { get; set; }
+        }
+        private static void ChangeByReference(ref Product itemRef)
+        {
+            // Change the address that is stored in the itemRef parameter.
+            itemRef = new Product("Stapler", 99999);
+
+            // You can change the value of one of the properties of
+            // itemRef. The change happens to item in Main as well.
+            itemRef.ItemID = 12345;
+        }
+        [Test]
+        public void UseRef_按引用传递参数示例2()
+        {
+            // Declare an instance of Product and display its initial values.
+            Product item = new Product("Fasteners", 54321);
+            System.Console.WriteLine("Original values in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+
+            // Pass the product instance to ChangeByReference.
+            ChangeByReference(ref item);
+            System.Console.WriteLine("Back in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+        }
+        //引用返回值示例
+        private static ref int Find(int[,] matrix, Func<int, bool> predicate)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (predicate(matrix[i, j]))
+                        return ref matrix[i, j];
+            throw new InvalidOperationException("Not found");
+        }
+        private class Book
+        {
+            public string Author;
+            public string Title;
+        }
+        private class BookCollection
+        {
+            private Book[] books = { new Book { Title = "Call of the Wild, The", Author = "Jack London" },
+                        new Book { Title = "Tale of Two Cities, A", Author = "Charles Dickens" }
+                       };
+            private Book nobook = null;
+
+            public ref Book GetBookByTitle(string title)
+            {
+                for (int ctr = 0; ctr < books.Length; ctr++)
+                {
+                    if (title == books[ctr].Title)
+                        return ref books[ctr];
+                }
+                return ref nobook;
+            }
+
+            public void ListBooks()
+            {
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"{book.Title}, by {book.Author}");
+                }
+                Console.WriteLine();
+            }
+        }
+        [Test]
+        public void UseRef_引用返回值示例()
+        {
+            //调用方将 GetBookByTitle 方法所返回的值存储为 ref 局部变量时，调用方对返回值所做的更改将反映在 BookCollection 对象中，如下例所示。
+            var bc = new BookCollection();
+            bc.ListBooks();
+
+            ref var book = ref bc.GetBookByTitle("Call of the Wild, The");
+            if (book != null)
+                book = new Book { Title = "Republic, The", Author = "Plato" };
+            bc.ListBooks();
+            // The example displays the following output:
+            //       Call of the Wild, The, by Jack London
+            //       Tale of Two Cities, A, by Charles Dickens
+            //
+            //       Republic, The, by Plato
+            //       Tale of Two Cities, A, by Charles Dickens
+        }
+        [Test]
+        public void UseRef_CS8171编译错误()
+        {
+            {//error
+                //int a = 123;
+                //ref int x = ref a;
+                //var y = ref x;
+            }
+            {//To correct this error
+                int a = 123;
+                ref int x = ref a;
+                var y = x;
+            }
+        }
+        private class MyClassCS0206Error
+        {
+            public static int P
+            {
+                get
+                {
+                    return 0;
+                }
+                set
+                {
+                }
+            }
+            public static void MyMethod(ref int i)
+            // public static void MyMethod(int i)  
+            {
+            }
+        }
+        private class MyClassCS0206ToCorrect
+        {
+            public static int P;
+            public static void MyMethod(ref int i)
+            // public static void MyMethod(int i)  
+            {
+            }
+        }
+        [Test]
+        public void UseRef_CS0206编译错误()
+        {
+            {
+                //MyClassCS0206Error.MyMethod(ref MyClassCS0206Error.P);   // CS0206  
+                // try the following line instead  
+                // MyMethod(P);   // CS0206  
+            }
+            {
+                MyClassCS0206ToCorrect.MyMethod(ref MyClassCS0206ToCorrect.P);   // CS0206  
+                // try the following line instead  
+                // MyMethod(P);   // CS0206  
+            }
+        }
+    }
+}
