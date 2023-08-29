89672cb7137c17e2cd1b9de493c8c26db51334e3
张少驰 <shaochi.zhang@capgemini.com>
2023-08-29 15:19:54 +0800

    feat: 新增FilterHandler示例。

--- SCscCL_MVC/Controllers/HomeController.cs
+++ SCscCL_MVC/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace SCscCL_MVC.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		public string TestApi()
+		{
+			return "TestApi";
+
+        }
 	}
 }
---
+++ SCscCL_MVC/WebInfra/Filter/Handler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SCscCL_MVC.WebInfra.Filter
+{
+    public class Handler : Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute
+	{
+        //1/5
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+        //2/5:Controller当中的逻辑
+        //3/5
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+        //4/5
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+        }
+        //5/5
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+    }
+}
