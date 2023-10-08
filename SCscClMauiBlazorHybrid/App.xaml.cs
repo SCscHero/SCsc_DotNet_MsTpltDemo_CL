aac7cc56b9a8e2d4373003eb7bd98bbbd4ed5d9a
张少驰 <shaochi.zhang@capgemini.com>
2023-10-08 11:03:14 +0800

    feat: 修改Maui工程命名以解决无法启动问题（The project needs to be deployed before we can debug. Please enable Deploy in the Configuration Manager.）。

---
+++ SCscClMauiBlazorHybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SCscClMauiBlazorHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
