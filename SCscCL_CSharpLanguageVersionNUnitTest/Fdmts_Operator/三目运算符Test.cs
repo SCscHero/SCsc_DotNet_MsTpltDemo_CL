cf9cdb99211724cbee8957fb5d23ad45d1575151
张少驰 <shaochi.zhang@capgemini.com>
2023-11-17 23:56:06 +0800

    feat: 新增运算符相关Demo。

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Operator/三目运算符Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CsLangVersion.Fdmts_Operator
+{
+	internal class 三目运算符Test
+	{
+		[Test]
+		public void 三目运算符嵌套()
+		{
+			var scsc = true ? (true || true) ? "AllTrue" : "OuterTrueAndInterLayerFalse"
+		: "AllFalse";
+			Console.WriteLine(scsc);
+		}
+	}
+}
