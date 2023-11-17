785f589a3e6aeefd3e6b7aa4b28819db05f402d8
张少驰 <shaochi.zhang@capgemini.com>
2023-11-18 00:36:19 +0800

    feat: 新增SystemMethodTest与typeof相关的Demo。

--- SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_SystemMethod/SystemMethodTest.cs
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_SystemMethod/SystemMethodTest.cs
@@ -3,38 +3,51 @@
 namespace CsLangVersion.Fdmts_SystemMethod
 {
 	internal partial class SystemMethodTest
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-        [Test]
-        public void 方法nameof测试()
-        {
-            Console.WriteLine(nameof(方法nameof测试));
-        }
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+		[Test]
+		public void 方法nameof测试()
+		{
+			Console.WriteLine(nameof(方法nameof测试));
+		}
 
-        [Test]
-        public void 方法typeof测试()
-        {
-            Console.WriteLine(typeof(int));
-        }
+		[Test]
+		public void 方法typeof测试()
+		{
+			Console.WriteLine(typeof(int?));
+			Console.WriteLine(typeof(Nullable<int>));
+			Console.WriteLine(typeof(Int32?));
+			Console.WriteLine(typeof(Int32?) == typeof(Nullable<int>) && typeof(Nullable<int>) == typeof(int?));
 
-        [Test]
-        public void 方法sizeof测试()
-        {
-            Console.WriteLine($@"bool sizeof:{sizeof(bool)}");
-            Console.WriteLine($@"sbyte sizeof:{sizeof(sbyte)}");
-            Console.WriteLine($@"byte sizeof:{sizeof(byte)}");
-            Console.WriteLine($@"char sizeof:{sizeof(char)}");
-            Console.WriteLine($@"short sizeof:{sizeof(short)}");
-            Console.WriteLine($@"ushort sizeof:{sizeof(ushort)}");
-            Console.WriteLine($@"int sizeof:{sizeof(int)}");
-            Console.WriteLine($@"uint sizeof:{sizeof(uint)}");
-            Console.WriteLine($@"float sizeof:{sizeof(float)}");
-            Console.WriteLine($@"double sizeof:{sizeof(double)}");
-            Console.WriteLine($@"Int64 sizeof:{sizeof(long)}");
-            Console.WriteLine($@"decimal sizeof:{sizeof(decimal)}");
-        }
-    }
+			Console.WriteLine(typeof(int));
+			Console.WriteLine(typeof(Int32));
+
+			Console.WriteLine(typeof(int) == typeof(Int32));
+
+			Console.WriteLine(typeof(string));
+			Console.WriteLine(typeof(String));
+			Console.WriteLine(typeof(string) == typeof(String));
+
+		}
+
+		[Test]
+		public void 方法sizeof测试()
+		{
+			Console.WriteLine($@"bool sizeof:{sizeof(bool)}");
+			Console.WriteLine($@"sbyte sizeof:{sizeof(sbyte)}");
+			Console.WriteLine($@"byte sizeof:{sizeof(byte)}");
+			Console.WriteLine($@"char sizeof:{sizeof(char)}");
+			Console.WriteLine($@"short sizeof:{sizeof(short)}");
+			Console.WriteLine($@"ushort sizeof:{sizeof(ushort)}");
+			Console.WriteLine($@"int sizeof:{sizeof(int)}");
+			Console.WriteLine($@"uint sizeof:{sizeof(uint)}");
+			Console.WriteLine($@"float sizeof:{sizeof(float)}");
+			Console.WriteLine($@"double sizeof:{sizeof(double)}");
+			Console.WriteLine($@"Int64 sizeof:{sizeof(long)}");
+			Console.WriteLine($@"decimal sizeof:{sizeof(decimal)}");
+		}
+	}
 }
