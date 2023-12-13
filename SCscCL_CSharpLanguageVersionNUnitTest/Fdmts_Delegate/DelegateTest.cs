b62d1014f980133ece5cecd54022b6ffd6ce27db
张少驰 <shaochi.zhang@capgemini.com>
2023-12-13 18:46:07 +0800

    feat: 新增Delegate关键字相关Demo；

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Delegate/DelegateTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLangVersion.Fdmts_Delegate
+{
+	internal class DelegateTest
+	{
+
+		public delegate void MyDelegate(string message);
+		public static void MethodToInvoke1(string message)
+		{
+			Console.WriteLine($"Method 1: {message}");
+		}
+		public static void MethodToInvoke2(string message)
+		{
+			Console.WriteLine($"Method 2: {message}");
+		}
+		public static void MethodToInvoke3(string message)
+		{
+			Console.WriteLine($"Method 3: {message}");
+		}
+		[Test]
+		public void delegate初使用()
+		{
+			MyDelegate delegate1 = MethodToInvoke1;
+			MyDelegate delegate2 = MethodToInvoke2;
+			MyDelegate delegate3 = MethodToInvoke3;
+			List<MyDelegate> delegateList = new List<MyDelegate> { delegate1, delegate2, delegate3 };
+			foreach (MyDelegate del in delegateList)
+			{
+				del("Hello from delegate");
+			}
+		}
+	}
+}
