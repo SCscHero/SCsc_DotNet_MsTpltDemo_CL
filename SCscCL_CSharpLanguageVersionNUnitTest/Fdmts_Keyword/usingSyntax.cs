b6f4fc1490b446f24a9e54b828f76668a00f7b77
张少驰 <shaochi.zhang@capgemini.com>
2023-12-14 19:52:43 +0800

    feat: 新增using文法示例；

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Keyword/usingSyntax.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace CsLangVersion.Fdmts_Keyword
+{
+	internal class usingSyntax
+	{
+		[Test]
+		public void using等价写法()
+		{
+
+			using (var stream1 = new FileStream("file.txt", FileMode.Open))
+			{
+
+			}// 这里会自动调用stream.Close()方法
+			 //⬇️⬇️⬆️⬆️“等价于”以下写法
+			Stream stream2 = null;
+			try
+			{
+				stream2 = new FileStream("file.txt", FileMode.Open);
+				// 读取或写入文件操作  
+			}
+			finally
+			{
+				if (stream2 != null)
+				{
+					stream2.Close(); // 确保关闭流对象  
+				}
+			}
+		}
+	}
+}
