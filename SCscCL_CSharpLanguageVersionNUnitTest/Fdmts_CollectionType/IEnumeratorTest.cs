f0c516f641be4e132ce6422142726b57561b077a
张少驰 <shaochi.zhang@capgemini.com>
2023-12-31 23:15:14 +0800

    feat: 新增集合操作stringArray和IEnumerator的UT；

--- SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_CollectionType/IEnumeratorTest.cs
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_CollectionType/IEnumeratorTest.cs
@@ -8,31 +8,37 @@ using System.Threading.Tasks;
 
 namespace CsLangVersion.Fdmts_CollectionType
 {
-    internal class IEnumeratorTest
-    {
+	internal class IEnumeratorTest
+	{
+		//【Ref】https://www.cnblogs.com/eve612/p/13628231.html
+		[Test]
+		public void IEnumerator_可以同时遍历多个集合并且节省内存用完一个释放一个()
+		{
 
-        [Test]
-        public void IEnumerator_使用示例()
-        {
-            List<int> intList = new List<int>();
-            for (int i = 0; i < 10; i++)
-            {
-                intList.Add(Get());
-            }
+		}
 
-            int Get()
-            {
-                Thread.Sleep(1000);
-                return DateTime.Now.Second;
-            }
+		[Test]
+		public void IEnumerator_使用示例()
+		{
+			List<int> intList = new List<int>();
+			for (int i = 0; i < 10; i++)
+			{
+				intList.Add(Get());
+			}
 
-            IEnumerator GetEnumerator()
-            {
-                for (int i = 0; i < intList.Count(); i++)
-                {
-                    yield return intList[i];
-                }
-            }
-        }
-    }
+			int Get()
+			{
+				Thread.Sleep(1000);
+				return DateTime.Now.Second;
+			}
+
+			IEnumerator GetEnumerator()
+			{
+				for (int i = 0; i < intList.Count(); i++)
+				{
+					yield return intList[i];
+				}
+			}
+		}
+	}
 }
---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_CollectionType/stringArray.cs
@@ -0,0 +1,24 @@
+﻿namespace CsLangVersion.Fdmts_CollectionType
+{
+	internal class stringArray
+	{
+		//【Ref】https://learn.microsoft.com/en-us/dotnet/csharp/how-to/concatenate-multiple-strings
+		[Test]
+		public void Concat方法()
+		{
+			string[] words = { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog." };
+
+			var unreadablePhrase = string.Concat(words);
+			System.Console.WriteLine(unreadablePhrase);
+
+			var readablePhrase = string.Join(" ", words);
+			System.Console.WriteLine(readablePhrase);
+		}
+		//【Ref】https://learn.microsoft.com/en-us/dotnet/csharp/how-to/concatenate-multiple-strings
+		[Test]
+		public void 官方示例()
+		{
+
+		}
+	}
+}
