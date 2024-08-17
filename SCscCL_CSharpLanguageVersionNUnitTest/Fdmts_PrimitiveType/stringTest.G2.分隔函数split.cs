ce42d8510af10f3199949b1606cea7d90bf19eeb
SCscHero <scschero@foxmail.com>
2024-08-17 17:25:49 +0800

    feat: The simplest split example of the string type extension method;

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_PrimitiveType/stringTest.G2.分隔函数split.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CsLangVersion.Fdmts_PrimitiveType {
+  internal partial class stringTest {
+    [Test]
+    public void split函数分隔不含分隔符的string () {
+      var emailTos = "scschero@foxmail.com".Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+      foreach(var item in emailTos) {
+        Console.WriteLine(item);
+        //output:scschero@foxmail.com
+      }
+    }
+
+  }
+}
