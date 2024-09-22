8f892a1416ea20ec0cb25bfbb6f2104a8a46f4bf
SCscHero <scschero@foxmail.com>
2024-09-22 21:06:02 +0800

    feat: Added more UT examples;

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_Generic/GenericTest.FS.CollectionsGeneric_集合泛型最简单示例.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CsLangVersion.Fdmts_Generic {
+  /// <summary>
+  /// GenericTest中文简称泛型
+  /// </summary>
+  public partial class GenericTest {
+    public class Bookshelf {
+      public IEnumerable Book { get; set; }
+    }
+    public class BookshelfWithGeneric {
+      public IEnumerable<Book> Book { get; set; }
+    }
+
+    public class Book {
+      public string BookName { get; set; }
+    }
+
+    [Test]
+    public void 泛型最基础使用_Demo1() {
+
+      System.Console.WriteLine(@$"UT Excuted {nameof(GenericTest)}_{nameof(泛型最基础使用_Demo1)}");
+
+      System.Console.WriteLine(@$"Finished local time is {DateTime.Now.ToString("HH:mm:ss")};");
+    }
+
+  }
+}
