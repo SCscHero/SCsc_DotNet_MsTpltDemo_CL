718515a93db40fe87e2ff589deb95daba065281c
SCscHero <scschero@foxmail.com>
2024-11-24 12:51:32 +0800

    feat: Added more UT examples;

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_PrimitiveType/EnumTest.00.Init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CsLangVersion.Fdmts_PrimitiveType
+{
+	internal partial class EnumTest
+	{
+
+		[Test]
+		public void Enum遍历Test()
+		{
+			foreach (DayOfWeek suit in Enum.GetValues(typeof(DayOfWeek)))
+			{
+				Console.WriteLine((int)suit + ":" + suit);
+			}
+		}
+		[Test]
+		public void string转enum()
+		{
+			Enum.TryParse("Monday", out DayOfWeek outE);
+			Assert.IsTrue(DayOfWeek.Monday==outE);
+		}
+
+		[Test]
+		public void Int转enum() {
+            var res1 = (DayOfWeek)1;
+			Assert.True(DayOfWeek.Monday==res1);
+		}
+
+
+	}
+}
---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_PrimitiveType/EnumTest.Ex.常见错误：system error Can't write CLR type SCModel.ProductType with handler type Int64Handler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Runtime.ConstrainedExecution;
+using System.Threading.Tasks;
+
+namespace CsLangVersion.Fdmts_PrimitiveType {
+    public enum ProductType {
+        Unknown = 0,
+        Electronics = 1,
+        Clothing = 2,
+        Food = 3
+    }
+
+    public class Product {
+        public int Id { get; set; }
+        public ProductType Type { get; set; }
+    }
+
+    [TestFixture]
+    internal partial class EnumTest {
+        [Test]
+        public void 枚举未显示转换Int报错() {
+            var product = new Product() {
+                Id=1,
+                Type=ProductType.Electronics
+            };
+            /*The SugarParameter type constructor accepts an object.As follows:*/
+            // public SugarParameter(string name, object value) {
+            //    Value=value;
+            //    ParameterName=name;
+            //    if(value!=null) {
+            //        SettingDataType(value.GetType());
+            //     }
+            //}
+
+            /*Enum types can also be passed in, but they will not be correctly converted to INT, and we will need to display the conversion, or throw an error if we don't: system error:Can't write CLR type Product.ProductType with handler type Int64Handler*/
+
+            //var productList = daoHelper.Context.Ado.SqlQuery<ProductEntity>(@"select * from Product where producttype=@ProductType", new SugarParameter[] {
+            //      new SugarParameter("@ProductType", product.Type),
+            //});
+        }
+    }
+}
