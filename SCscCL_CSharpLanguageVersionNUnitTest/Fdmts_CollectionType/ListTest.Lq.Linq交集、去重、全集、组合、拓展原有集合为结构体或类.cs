b5b26c8e2ad721f82860095d65cda30ed935c4a2
SCscHero <39325371+SCscHero@users.noreply.github.com>
2025-02-16 17:45:35 +0800

    feat: Added more UT examples;

---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_CollectionType/ListTest.Lq.Linq交集、去重、全集、组合、拓展原有集合为结构体或类.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsLangVersion.Fdmts_CollectionType {
+    public partial class ListTest {
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    [Test]
+    public void List_Union_Test() {
+      var list1 = new List<int>() { 0, 5, 7, 1, 3, 4 };
+      var list2 = new List<int>() { 1, 3, 5, 0, 0, 7 };
+
+      var resUnion = list1.Union(list2).ToList();
+      var resConcat = list1.Concat(list2).ToList();
+      Console.ReadLine();
+    }
+
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    [Test]
+    public void List_Distinct_Test() {
+      IList<int> intList1 = new List<int>() { 0, 5, 7, 1, 3, 4 };
+      IList<int> intList2 = new List<int>() { 1, 3, 5, 0, 0, 7 };
+
+      var IntersectList = intList1.Intersect(intList2);
+
+      //var res1 = decimal.Compare(dec1, dec2);//0
+      //var res2 = decimal.Compare(dec2, dec2);//0
+      //var res3 = decimal.Compare(dec1, dec3);//-1
+      Console.ReadLine();
+    }
+
+    #region TestModel
+    private struct MoreStruct {
+      public int intVal;
+      public string strVal;
+    }
+
+    private class MoreClass {
+      public int intVal;
+      public string strVal;
+    }
+    #endregion
+
+    /// <summary>
+    /// listLInq一对多列
+    /// </summary>
+    [Test]
+    public void List_EleOne2More_Test() {
+      //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/select-clause?f1url=%3FappId%3DDev16IDEF1%26l%3DEN-US%26k%3Dk(select_CSharpKeyword)%3Bk(SolutionItemsProject)%3Bk(DevLang-csharp)%26rd%3Dtrue
+
+      List<int> needToTransformedList = new List<int>() { 0, 5, 7, 1, 3, 4, 6, 8, 2, 0, 5, 6, 7 };
+      var moreAnonymousArray = (from list in needToTransformedList
+                                select new { queryInt = list, strEmpty = "" }).ToArray();
+      for(int i = 0; i<moreAnonymousArray.Count(); i++) {
+        //moreAnonymousArray[i].strEmpty = $@"Custom";
+      }
+
+      //选择1：Linq转换成匿名对象，但注意该集合是只读的。
+      var moreAnonymousList = (from list in needToTransformedList
+                               select new { queryInt = list, strEmpty = "" }).ToList();
+      for(int i = 0; i<moreAnonymousList.Count(); i++) {
+        //moreAnonymousList[i] with { strEmpty = "xxx"};
+        //Error CS0201  Only assignment, call, increment, decrement, await, and new object expressions can be used as a statement
+
+
+
+        //moreAnonymous[i].strEmpty = "xxx";
+        //Error CS0021  Cannot apply indexing with[] to an expression of type 'IEnumerable<<anonymous type: int queryInt, string strEmpty>>'  
+        //Error CS0200  Property or indexer '<anonymous type: int queryInt, string strEmpty>.strEmpty' cannot be assigned to --it is read only
+      }
+      ;
+      //转换为Class
+      var moreClass = (from list in needToTransformedList
+                       select new MoreClass { intVal=list, strVal="" }).ToList();
+      for(int i = 0; i<moreClass.Count; i++) {
+        moreClass[i].strVal=$"intVal={moreClass[i].intVal.ToString()};";
+      }
+      //compile error:转换为Struct
+      var moreStructToList = (from list in needToTransformedList
+                              select new MoreStruct { intVal=list, strVal="" }).ToList();
+      for(int i = 0; i<moreStructToList.Count; i++) {
+        ////compile error CS1612:Cannot modify the return value of 'List<ListTest.MoreStruct>.this[int]' because it is not a variable
+        //moreStructToList[i].strVal = $"intVal={moreStructToList[i].intVal.ToString()};";
+      }
+      //转换为Struct的Array
+      var moreStructArray = (from list in needToTransformedList
+                             select new MoreStruct { intVal=list, strVal="" }).ToArray();
+      for(int i = 0; i<moreStructArray.Count(); i++) {
+        moreStructArray[i].strVal=$"intVal={moreStructArray[i].intVal.ToString()};";
+      }
+
+      var dic = needToTransformedList.ToDictionary(r => System.Guid.NewGuid().ToString("D"), r => new MoreClass() { intVal=r, strVal=$"{r.ToString()}'s String" });
+      var dic1 = needToTransformedList.ToDictionary(r => System.Guid.NewGuid().ToString("D"), r => new { intVal = r, strVal = $"{r.ToString()}'s String" });
+
+      Console.WriteLine();
+    }
+
+
+    /// <summary>
+    /// 取交集
+    /// </summary>
+    [Test]
+    public void List_Intersect_Test() {
+      IList<int> listInt1 = new List<int>() { 0, 5, 7, 1, 3, 4 };
+      IList<int> listInt2 = new List<int>() { 1, 3, 5, 0, 0, 7 };
+
+      var IntersectList = listInt1.Intersect(listInt2);
+
+      //var res1 = decimal.Compare(dec1, dec2);//0
+      //var res2 = decimal.Compare(dec2, dec2);//0
+      //var res3 = decimal.Compare(dec1, dec3);//-1
+      Console.ReadLine();
+    }
+  }
+}
---
+++ SCscCL_CSharpLanguageVersionNUnitTest/Fdmts_CollectionType/ListTest.Lq.对象下的成员对象集合铺平展示.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CsLangVersion.Fdmts_CollectionType.ListTest.PersonHaveFriend;
+
+namespace CsLangVersion.Fdmts_CollectionType {
+    public partial class ListTest {
+        public class PersonHaveFriend {
+            public enum FriendEnum {
+                T0, T1
+            }
+            public class Friend {
+                public FriendEnum FriendEm { get; set; }
+                public string Alias { get; set; }
+            }
+            public List<Friend> Friends { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        [Test]
+        public void 使用Linq_SelectMany实现成员集合铺平() {
+            System.Console.WriteLine(@$"UT Excuted {nameof(ListTest)}_{nameof(使用Linq_SelectMany实现成员集合铺平)}");
+            var listPerson = new List<PersonHaveFriend> {
+                new PersonHaveFriend { Name="SCsc",Age=18, Friends=new List<Friend>
+                {
+                    new Friend{ FriendEm=FriendEnum.T0,Alias="MyFriendNb" },
+                    new Friend{ FriendEm=FriendEnum.T1,Alias="MyFriendDog" },
+                } }
+            };
+            var result = listPerson
+    .SelectMany(p => p.Friends, (person, friend) => new {
+        person.Name,
+        person.Age,
+        FriendType = friend.FriendEm,
+        Alias = friend.Alias
+    })
+    .ToList();
+            System.Console.WriteLine(@$"Finished local time is {DateTime.Now.ToString("HH:mm:ss")};");
+        }
+
+    }
+}
